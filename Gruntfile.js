6242b3a70fdb71cbbc3be25001a5f3ab9aeda755
ekhaled <me.khaled@gmail.com>
2015-05-18 19:52:27 +0100

    add some linting and test stubs

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,14 +4,44 @@ var path = require('path');
 
 module.exports = function(grunt){
 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
   grunt.loadTasks('tasks');
 
   grunt.initConfig({
+
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>',
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
+
     ractive:{
-      src: '',
-      dest: ''
-    }
+      main:{
+        options:{
+          type: 'cjs'
+        },
+        files:{
+          'test/dest/': 'test/src/*'
+        }
+      }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js'],
+    },
+
   });
+
+  grunt.registerTask('default', ['jshint']);
 
 
 }
